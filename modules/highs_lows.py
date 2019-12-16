a63ed85d9b205d8c7aa1745fa0fd25446106e2e5
abhiandthetruth <abhiandthetruth@gmail.com>
2019-12-16 22:11:46 +0530

    structure changes and saving plots added

--- modules/highs_lows.py
+++ modules/highs_lows.py
@@ -1,22 +1,25 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime 
-filename = 'data\\sitka_weather_07-2014.csv'
+filename = 'data\\sitka_weather_2014.csv'
 
 with open(filename) as f:
     #getting headers
     reader = csv.reader(f)
     header_row = next(reader)
     #getting the high temps with dates
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         highs.append(int(row[1]))
+        lows.append(int(row[3]))
         dates.append(datetime.strptime(row[0], "%Y-%m-%d"))
     #plotting
     fig = plt.figure(figsize=(10, 6))
     plt.plot(dates, highs, c='red')
-    plt.title("Daily high temperatures, July 2014", fontsize=14)
+    plt.plot(dates, lows, c='blue')
+    plt.title("Daily high and low temperatures - 2014", fontsize=14)
     plt.xlabel("Days", fontsize=10)
     plt.ylabel("Temperature(F)", fontsize=10)
     plt.tick_params(axis='both', which='major', labelsize=8)
+    plt.savefig('visualizations\\sitka_weather_2014_plot.png', bbox_inches='tight')
     plt.show()
--- modules/rw_visual.py
+++ modules/rw_visual.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
+from datetime import datetime
 
 c = 'y'
 while c=='y': 
     rw = RandomWalk(50000)
     rw.fill_walk()
+    now = str(datetime.now()).split(".")[0].split(" ")
+    time = now[1].split(":")
+    name = ("visualizations\\rw_plot_" + now[0] + "_" + time[0] + 
+        time[1] + ".png") 
     plt.figure(figsize=(10, 6))
     pts = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=pts,
@@ -14,5 +19,6 @@ while c=='y':
         edgecolors='none', s=100)
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
+    plt.savefig( name)
     plt.show()
     c = input("Another(y/n): ")
