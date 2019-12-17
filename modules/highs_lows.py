8c9f40e7e1e986916c6e0fedb2670b85e02314d8
abhiandthetruth <abhiandthetruth@gmail.com>
2019-12-17 18:59:26 +0530

    added death valley with missing data correction

--- modules/highs_lows.py
+++ modules/highs_lows.py
@@ -1,25 +1,37 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime 
-filename = 'data\\sitka_weather_2014.csv'
 
-with open(filename) as f:
+filename = 'death_valley_2014.csv'
+
+fullfilename = 'data\\' + filename
+
+with open(fullfilename) as f:
     #getting headers
     reader = csv.reader(f)
     header_row = next(reader)
     #getting the high temps with dates
     dates, highs, lows = [], [], []
     for row in reader:
-        highs.append(int(row[1]))
-        lows.append(int(row[3]))
-        dates.append(datetime.strptime(row[0], "%Y-%m-%d"))
+        try:
+            date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3]) 
+        except ValueError:
+            print(date, 'missing data')
+        else:
+            dates.append(date)
+            highs.append(high)
+            lows.append(low)
     #plotting
     fig = plt.figure(figsize=(10, 6))
-    plt.plot(dates, highs, c='red')
-    plt.plot(dates, lows, c='blue')
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
     plt.title("Daily high and low temperatures - 2014", fontsize=14)
     plt.xlabel("Days", fontsize=10)
+    fig.autofmt_xdate()
     plt.ylabel("Temperature(F)", fontsize=10)
     plt.tick_params(axis='both', which='major', labelsize=8)
-    plt.savefig('visualizations\\sitka_weather_2014_plot.png', bbox_inches='tight')
+    plt.savefig('visualizations\\'+filename.split(".")[0]+'_plot.png', bbox_inches='tight')
     plt.show()
